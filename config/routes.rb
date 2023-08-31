8dd38d42476579983db4c809649c3f12766319f0
tsubi-kaiju <136160024+tsubi-kaiju@users.noreply.github.com>
2023-09-01 08:31:58 +0900

    Revert "モデル名の変更"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'products#index'
-  resources :products, only: [:index]
+  root to: 'items#index'
+  resources :items, only: [:index]
 end
