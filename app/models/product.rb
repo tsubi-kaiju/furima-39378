c3ff1c32bd4ec5545f043de610ef281b01232d28
tsubi-kaiju <tsugu.no.1@gmail.com>
2023-09-23 19:26:22 +0900

    Productモデルの選択肢とバリデーションを実装
    コード整形のため、rubocop -aも実行済。

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,25 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :cateogory
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_duration
+  belongs_to :shipping_fee_bearer
+  belongs_to :shipping_origin
+
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price,
+              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
+              format: { with: /\A[0-9]+\z/}
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :shipping_fee_bearer_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :shipping_origin_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :shipping_duration_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :image
+  end
 end
